3719e9e03d83f2f70cc0367344262169f817f618
bagusnugrohow20 <papaobobo@gmail.com>
2024-03-01 04:34:43 +0700

    test 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full top-0'>
       <div className='max-w-[1300px] px-4 flex justify-between mx-auto py-4 bg-opacity-60 bg-[#000]'>
-        <Link to='home' smooth={true} duration={500} offset={-300}><h1 className='text-slate-300 text-xl md:text-3xl cursor-pointer'>LINA & VIAN</h1></Link>
+        <Link to='home' smooth={true} duration={500} offset={-300}><h1 className='text-slate-300 text-xl md:text-3xl cursor-pointer'>Bagus Nugroho</h1></Link>
         <ul className='gap-x-5 hidden sm:flex'>
           <Link to='about' smooth={true} duration={500} offset={-100}><li className='text-slate-300 text-xl md:text-2xl hover:text-blue-500 block transition hover:-skew-y-3 cursor-pointer'>About</li></Link>
           <Link to='certificate' smooth={true} duration={500} offset={-100}><li className='text-slate-300 text-xl md:text-2xl hover:text-blue-500 block transition hover:-skew-y-3 cursor-pointer'>Certificates</li></Link>
